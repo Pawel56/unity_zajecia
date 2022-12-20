423b92e7e87b4f9bfe86c00b2402aeeae666a117
Paweł Janicki <59371732+Pawel56@users.noreply.github.com>
2022-12-20 17:16:07 +0100

    Add files via upload

---
+++ lab4_zad1.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RandomCubesGenerator : MonoBehaviour
+{
+    List<Vector3> positions = new List<Vector3>();
+    public float delay = 3.0f;
+    int objectCounter = 0;
+    // obiekt do generowania
+    public GameObject block;
+    public int blocks = 10;
+    public Material material1;
+    public Material material2;
+    public Material material3;
+    public Material material4;
+    public Material material5;
+
+    void Start()
+    {
+        // w momecie uruchomienia generuje 10 kostek w losowych miejscach
+        List<int> pozycje_x = new List<int>(Enumerable.Range((int)transform.position.x, (int)transform.position.x + 10).OrderBy(x => Guid.NewGuid()).Take(blocks));
+        List<int> pozycje_z = new List<int>(Enumerable.Range((int)transform.position.z, (int)transform.position.z + 10).OrderBy(x => Guid.NewGuid()).Take(blocks));
+
+        for (int i = 0; i < 10; i++)
+        {
+            this.positions.Add(new Vector3(pozycje_x[i], 5, pozycje_z[i]));
+        }
+        foreach (Vector3 elem in positions)
+        {
+            Debug.Log(elem);
+        }
+        // uruchamiamy coroutine
+        StartCoroutine(GenerujObiekt());
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator GenerujObiekt()
+    {
+        Debug.Log("wywo³ano coroutine");
+        foreach (Vector3 pos in positions)
+        {
+            Instantiate(this.block, this.positions.ElementAt(this.objectCounter++), Quaternion.identity);
+            yield return new WaitForSeconds(this.delay);
+        }
+        // zatrzymujemy coroutine
+        StopCoroutine(GenerujObiekt());
+    }
+}
